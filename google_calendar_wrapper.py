c366106a6426013de39d14c26b18d8e1a6092203
Laureen Schausberger <laureen.schausberger@students.fh-hagenberg.at>
2017-06-05 16:02:20 +0200

    init

---
+++ google_calendar_wrapper.py
@@ -0,0 +1,29 @@
+from apiclient import discovery
+
+from flask_ask import session
+
+from oauth2client.client import AccessTokenCredentials
+from name_email import name_email_dict
+
+import httplib2
+
+
+def get_service():
+    credentials = get_user_credentials()
+    http = credentials.authorize(httplib2.Http())
+    return discovery.build('calendar', 'v3', http=http)
+
+
+def get_user_credentials():
+    if hasattr(session.user, 'accessToken') and session.user.accessToken is not None:
+        return AccessTokenCredentials(session.user.accessToken, "Alexa/1.0")
+    else:
+        return None
+
+
+#get the calendars depending on the person
+def get_calendar(name):
+    if not(name in name_email_dict):
+        return False
+    else:
+        return name_email_dict[name]
---
+++ name_email.py
@@ -0,0 +1,5 @@
+name_email_dict = {
+    'Nina': 'nina@calvifamily.org',
+    'Theodora': 'zamfirescu.teodora@gmail.com',
+    'Elizabeth': 'elisabeth.m.egger@gmail.com'
+}
